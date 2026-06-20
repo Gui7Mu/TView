a5fccd2d971d363a0fe66f56a10a20fa7f37929f
Gui7Mu <guidomusico37@gmail.com>
2026-06-21 00:40:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE = 'mda-v7';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE='mda-v13';const ASSETS=['./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
